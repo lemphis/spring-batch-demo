70d2fcab1f088bde67fd1d23318500a11a9c604d
lemphis <lemphis@gmail.com>
2023-02-14 21:30:02 +0900

    Initial commit

---
+++ src/test/kotlin/me/lemphis/springbatchdemo/SpringBatchDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.lemphis.springbatchdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
